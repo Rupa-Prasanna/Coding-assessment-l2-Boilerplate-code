e59f649da32f85d7eb49f2784c4612a56417b4ee
Rupa-Prasanna <137879356+Rupa-Prasanna@users.noreply.github.com>
2024-04-27 07:53:57 +0000

    commit1 by rupa

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log('====================================');
 console.log("Connected");
 console.log('====================================');
+let addtocart=document.getElementById("addtocart");
+let message=document.getElementById("confirmation-message");
+
+
+addtocart.addEventListener("click", function(){
+    message.style.display="block";
+});
